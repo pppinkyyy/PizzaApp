924cdd2a2d74a0fe173485ca8df0510938327d6c
Alia <pppinkyyy7@gmail.com>
2023-11-15 23:03:02 +0200

    Initial Commit

---
+++ PizzaApp/PizzaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaAppApp.swift
+//  PizzaApp
+//
+//  Created by Alia on 15.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct PizzaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
