bd3f9df5414d03077c6d4ee4611593c3b112c60c
Alia <pppinkyyy7@gmail.com>
2023-12-19 23:45:41 +0200

    Make screen "CartView"

---
+++ PizzaApp/View/Components/PositionCell.swift
@@ -0,0 +1,18 @@
+//
+//  PositionCellView.swift
+//  PizzaApp
+//
+//  Created by Alia on 23.11.2023.
+//
+
+import SwiftUI
+
+struct PositionCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PositionCellView()
+}
